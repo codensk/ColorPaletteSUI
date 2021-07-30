b6a90b283330f77ed90e3a292391a982c73cb0f0
Sergey Vorobev <rusbear28@yandex.ru>
2021-07-30 09:07:17 +0700

    Initial Commit

---
+++ ColorPaletteSUI/ColorPaletteSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPaletteSUIApp.swift
+//  ColorPaletteSUI
+//
+//  Created by SERGEY VOROBEV on 30.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorPaletteSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
